a2dfef1b8b5157f08f32ef94d4bdee192b8e4627
takshpatel28 <takshpatel416@gmail.com>
2025-04-12 14:48:45 +0530

    updates

--- src/utiles/smoothScroll.jsx
+++ src/utiles/smoothScroll.jsx
@@ -1,9 +1,9 @@
 export const smoothScrollTo = (targetId) => {
-    const targetElement = document.querySelector(targetId);
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop - 100, // Adjust offset as needed
-        behavior: 'smooth'
-      });
-    }
-  };
+  const targetElement = document.querySelector(targetId);
+  if (targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop - 100, // Adjust offset as needed
+      behavior: 'smooth'
+    });
+  }
+};
