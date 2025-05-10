adc7d4fcdf27c4a0461fd7baf2aa61ffd0e21591
unknown <jeelpatel98796@gmail.com>
2025-05-10 14:28:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import AllRoutes from './AllRoutes'
 import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
+import ScrollToTop from './utiles/ScrollToTop'
 
 function App() {
 
   return (
     <div className="App bg-[#F6F5EC] min-h-screen flex flex-col justify-between">
+      <ScrollToTop />
       <Navbar />
       <AllRoutes />
       <Footer />
